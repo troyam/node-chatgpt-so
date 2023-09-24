0fea066e77985d32e5fb7b4b8e4e9c7935659ccc
Marcos Porto <marcosporto99@gmail.com>
2023-09-24 11:17:39 -0300

    Update chatgpt.js

--- chatgpt.js
+++ chatgpt.js
@@ -17,14 +17,19 @@ const api = new ChatGPTAPI({
 })
 
 
+/*
+Esse código envia uma mensagem usando uma API. Ele utiliza a função sendMessage da API, passando a string da mensagem como argumento. Além disso, ele também utiliza um objeto como segundo argumento, onde é definido o parentMessageId como temp.
+
+O parentMessageId é usado para associar a nova mensagem a uma mensagem pai, geralmente utilizado em threads de mensagens.
+
+O trecho de código comentado onProgress parece estar sendo utilizado para imprimir uma resposta parcial no console a cada vez que a função sendMessage envia uma parte da mensagem em progresso. Porém, ele está atualmente comentado e não está sendo executado.
+*/
 async function ask(string, cb) {
+   
 
-
-    const res = await api.sendMessage(string, {
-        onProgress: (partialResponse) => {
-            //console.log(partialResponse.text) }
-        }
-    });
+    const res = await api.sendMessage(string, {   onProgress: (partialResponse) => {  
+      //console.log(partialResponse.text) }
+      } } );
     let obj = {
         cmd: string,
         ...res
@@ -32,12 +37,28 @@ async function ask(string, cb) {
     console.log(chalk.greenBright(obj.text));
     cb(obj)
 }
-async function askParent(string, cb, temp) {
+
+
+
+/*
+O código em questão define uma função assíncrona chamada askParent. Essa função recebe três parâmetros: string, cb e temp.
+
+Dentro da função, o código realiza as seguintes ações:
+
+Chama uma função api.sendMessage passando a string e um objeto como argumento.
+O objeto possui uma propriedade parentMessageId definida como o valor do parâmetro temp.
+Aguarda a resposta da função api.sendMessage através da palavra-chave await e armazena o resultado na constante res.
+Cria um novo objeto chamado obj que contém a propriedade cmd definida como o valor do parâmetro string e todas as propriedades do objeto res.
+Imprime a propriedade text do objeto obj no console, com a cor verde brilhante utilizando a biblioteca chalk.
+Chama a função cb passando o objeto obj como argumento.
+Em resumo, essa função envia uma mensagem para api, aguarda a resposta e retorna um objeto contendo a mensagem enviada e a resposta recebida. Em seguida, chama uma função de callback cb com esse objeto como argumento.
+*/
+async function askParent(string, cb , temp) {
 
     const res = await api.sendMessage(string, {
         parentMessageId: temp,
-        //   onProgress: (partialResponse) => console.log(partialResponse.text)
-    });
+     //   onProgress: (partialResponse) => console.log(partialResponse.text)
+      });
     let obj = {
         cmd: string,
         ...res
@@ -46,17 +67,27 @@ async function askParent(string, cb, temp) {
     cb(obj)
 }
 
+/*
+Este código JavaScript é uma função assíncrona auto-executável (IIFE). O código dentro dessa função consiste em um loop interativo para receber dados de entrada do usuário usando a biblioteca pkg.
 
+O código começa definindo uma matriz vazia chamada history para armazenar mensagens. Em seguida, é definida uma função assíncrona chamada loop que recebe um parâmetro temp.
+
+Dentro da função loop, o código solicita ao usuário para digitar um comando usando pkg.question. Se houver um comando digitado, ele invoca uma função assíncrona chamada askParent e aguarda sua resposta usando await. Essa resposta é usada para preencher um objeto de mensagem que é adicionado à matriz history. Em seguida, o valor temp é atualizado com o ID retornado pela função askParent. Por fim, o código chama recursivamente a função loop passando o novo valor temp.
+
+Após a definição da função loop, o código fora da função principal, solicita novamente ao usuário para digitar um comando usando pkg.question. Se houver um comando digitado, ele invoca uma função assíncrona chamada ask e aguarda sua resposta usando await. Esta resposta também é usada para preencher um objeto de mensagem e adicioná-lo à matriz history. Da mesma forma que no loop, o valor temp é atualizado com o ID retornado pela função ask. Em seguida, a função loop é chamada, passando o valor temp.
+
+Em resumo, esse código realiza um loop interativo que solicita comandos ao usuário e mantém um registro de todas as mensagens e respostas recebidas.
+*/
 (async () => {
 
 
     const history = [];
-    async function loop(temp) {
+    async function loop (temp){
         const cmd = pkg.question("Type: \n");
 
-
-        if (cmd) {
-
+    
+        if(cmd){
+           
             await askParent(cmd, async (cb) => {
                 messages.push({
                     role: "user",
@@ -64,7 +95,7 @@ async function askParent(string, cb, temp) {
                     type: 'ask',
                     event: cmd,
                     res: cb.text,
-                    id: cb.id
+                    id: cb.id 
                 });
                 temp = cb.id
                 //console.log(messages)
@@ -72,13 +103,13 @@ async function askParent(string, cb, temp) {
             }, temp)
         }
     }
-
+    
     const cmd = pkg.question("Type: \n");
 
 
     var temp = '';
-    if (cmd) {
-
+    if(cmd){
+       
         await ask(cmd, async (cb) => {
             messages.push({
                 role: "user",
@@ -86,7 +117,7 @@ async function askParent(string, cb, temp) {
                 type: 'ask',
                 event: cmd,
                 res: cb.text,
-                id: cb.id
+                id: cb.id 
             });
             temp = cb.id
             //console.log(messages)
@@ -97,3 +128,5 @@ async function askParent(string, cb, temp) {
 
 
 })();
+
+
